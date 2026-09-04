07335eface8edbc1024f7d1792e26ce1644c8cfa
Nomu1Nomu1 <isaiah.tino@gmail.com>
2026-09-04 19:34:19 +0700

    hehe

---
+++ Scene/Main/Level/level_1.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@onready var spawn_point: Marker2D = $SpawnPoint
+
+# Path file scene karakter
+const PLAYER_SCENE = preload("res://Sprites/player_sword.tscn")
+
+func _ready() -> void:
+	_spawn_player()
+
+func _spawn_player() -> void:
+	if not PLAYER_SCENE:
+		push_error("Error: Scene res://player.tscn tidak ditemukan!")
+		return
+		
+	# Instantiate player dan posisikan di SpawnPoint
+	var player = PLAYER_SCENE.instantiate()
+	player.global_position = spawn_point.global_position
+	add_child(player)
+	
